3d026a7b655d2d8a0993d88f0ce859fe4495ecdb
Josh Delgadillo <delgadillo.i.joshua@gmail.com>
2020-11-30 21:18:24 -0800

    Attempted to start landing page

--- routes/modules.js
+++ routes/modules.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const modules = require('../config/moduleData');
 
 router.get('/:moduleId', (req, res) => {
-    res.render('module', { module: modules[req.params.moduleId] });
+    if(req.params.moduleId >= 0 && req.params.moduleId < modules.length)
+        res.render('module', { module: modules[req.params.moduleId] });
+    else
+        res.status(404).send("That page doesn't seem to exist :(");
 })
 
 module.exports = router;
